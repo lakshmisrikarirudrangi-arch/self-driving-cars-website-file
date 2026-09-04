07307fe8dc332e53444041b1b0c2fca594fa55a6
LakshmiRudrangi <lakshmisrikarirudrangi@gmail.com>
2026-09-04 02:59:56 +0000

    changed colors again

--- src/pages/Economic.jsx
+++ src/pages/Economic.jsx
@@ -1,9 +1,25 @@
 function Economic() {
   return (
     <section className="economic">
-      <h2> Economic Impacts of Autonomous Cars</h2>
-      <p> economic impacts </p>
+      <h2>Economic Impacts of Autonomous Cars</h2>
 
+      <div className="impacts-container3">
+        <div className="positive">
+          <h3>Positive Impacts</h3>
+          <p>
+           Self-driving cars would boost the economy by creating new jobs in technology, manufacturing, and maintenance. They can also reduce costs associated with transportation, accidents, insurance, and traffic congestion, while increasing productivity by allowing passengers to work or relax during travel.
+           Technology companies, automakers, and energy providers may earn more money from the demand for AI, sensors, software, and charging infrastructure. It also makes a more safe and efficient travel for the users.
+          </p>
+        </div>
+
+        <div className="negative">
+          <h3>Negative Impacts</h3>
+          <p>
+            Self-driving cars may lead to job losses in driving-related industries, such as taxi and truck drivers, and may require significant investment in infrastructure and technology. They can also create economic disparities between those who can afford autonomous vehicles and those who cannot, potentially exacerbating social inequality.
+            Materials such lithium, cobalt, and other earth metals used in batteries and sensors would be raised in cost and strain resources. 
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
